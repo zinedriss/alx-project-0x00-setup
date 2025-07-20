05c40a2e565c972831b833f5191647eff86351df
zinedriss <ssenuoy.issiurd@gmail.com>
2025-07-20 20:14:30 +0000

    img

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,9 @@
+const Pill: React.FC = () => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">Title</p>
+    </div>
+  )
+}
+
+export default Pill;
