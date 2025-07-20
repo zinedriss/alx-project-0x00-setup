fc23e359fdd24520574fc2f466d7d7c41f239494
zinedriss <ssenuoy.issiurd@gmail.com>
2025-07-20 20:24:31 +0000

    iplll

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,8 @@
-const Pill: React.FC = () => {
+import { PillProps } from "@/interfaces";
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      <p className=" text-sm ">{title}</p>
     </div>
   )
 }
