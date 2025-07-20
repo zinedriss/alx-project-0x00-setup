5fadaf677eee2d2a7eb9710d5a038e3ea8b01739
zinedriss <ssenuoy.issiurd@gmail.com>
2025-07-20 20:17:01 +0000

    card file

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,9 @@
+import Card from "@/components/Card"
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+    <Card />
     </div>
   )
 }
